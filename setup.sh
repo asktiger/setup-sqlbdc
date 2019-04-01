5a1ee58ad1b097587d43eb8ce41c7d82e3afefa0
asktiger <ask_tiger@hotmail.com>
2019-04-01 13:32:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 IP=(10.106.12.111 10.106.12.112 10.106.12.113 10.106.12.114)
-for x in ${IP[*]}; do ssh-copy-id -f $x; done
+for x in ${IP[*]}; do yes | ssh-copy-id -f $x; done
 
 cd /etc/ansible
-ansible-playbook -i hosts main.yml -b -v
+ansible-playbook -b -i hosts main.yml
 
 cd /tmp/kubernetes-installation-template
 bash start.sh
